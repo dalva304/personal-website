c2467fe841e9b5614892b8e4ae8b450e1692bc76
dalva304 <dianaa.alvarez03s@gmail.com>
2024-10-15 23:22:28 -0500

    changes in js

--- app.js
+++ app.js
@@ -28,4 +28,38 @@ var intro = {
   
   var typing = new Typed(".typed-text", intro);
 
-  
+// ScrollReveal from the top
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(".featured-name", { delay: 100 });
+sr.reveal(".text-info", { delay: 200 });
+sr.reveal(".text-btn", { delay: 200 });
+sr.reveal(".social_icons", { delay: 200 });
+sr.reveal(".project-box", { interval: 200 });
+sr.reveal(".top-header");
+
+// ScrollReveal from the left
+const srL = ScrollReveal({
+    origin: "left",
+    distance: "80px",
+    duration: 2000,
+    reset: true
+});
+
+srL.reveal(".some-element-left");
+
+// ScrollReveal from the right
+const srR = ScrollReveal({
+    origin: "right",
+    distance: "80px",
+    duration: 2000,
+    reset: true
+});
+
+srR.reveal(".some-element-right", { delay: 200 });
+
